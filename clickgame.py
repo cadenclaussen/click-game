735ff20d92f15986a0b89b6d58b88ea61e32c76e
Shane Claussen <claussen@us.ibm.com>
2020-05-09 17:00:54 -0700

    Minor tweaks

--- clickgame.py
+++ clickgame.py
@@ -2,43 +2,43 @@ import turtle
 import random
 import time
 
-color_list = ["black", "orange", "purple", "blue", "red"]
-shape_list = ["triangle", "circle", "square", "rectangle"]
-legnth_list = [20, 75, 50, 24, 60]
+colors = ["black", "orange", "purple", "blue", "red"]
+shapes = ["triangle", "circle", "square", "rectangle"]
+lengths = [20, 75, 50, 24, 60]
 
 winner = False
 pen = turtle.Pen()
 pen.shape("turtle")
-pen.reset()
 pen.speed(0)
 
 
 def draw():
-    global pen
-    global shape_list
-    global length_list
-    global color_list
-    pen.color(random.choice(color_list))
-    shape = random.choice(shape_list)
-    length = random.choice(length_list)
+    global pen, colors, shapes, lengths
+
+    pen.color(random.choice(colors))
+    shape = random.choice(shapes)
+    length = random.choice(lengths)
+
     if shape == "triangle":
-        pen.begin_fill()
         pen.begin_fill()
         for _ in range(3):
             pen.forward(200)
             pen.left(120)
         pen.end_fill()
-    if shape == "circle":
+
+    elif shape == "circle":
         pen.begin_fill()
         pen.circle(length)
         pen.end_fill()
-    if shape == "square":
+
+    elif shape == "square":
         pen.begin_fill()
         for _ in range(4):
             pen.forward(length)
             pen.left(90)
         pen.end_fill()
-    if shape == "rectangel":
+
+    elif shape == "rectangle":
         if length < 50:
             pen.begin_fill()
             for _ in range(2):
